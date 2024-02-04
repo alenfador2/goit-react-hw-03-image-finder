27a49f340fb839b12391d4ce78ae3c064834b0d8
alenfador2 <129333585+alenfador2@users.noreply.github.com>
2024-02-04 23:39:32 +0100

    Update 1.0 start project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import axios from 'axios';
+
+const baseURL = 'https://pixabay.com/api/';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchQuery: '',
+      page: 1,
+      per_page: 12,
+      key: '41281960-4f851dde922e1c31c346e4445',
+      orientation: 'horizontal',
+      image_type: 'photo',
+      isLoading: false,
+      loadMoreBtn: false,
+    };
+  }
+  componentDidMount() {
+    axios.get(baseURL, this.state).then();
+  }
+
+  render() {
+    return <></>;
+  }
+}
+export default App;
